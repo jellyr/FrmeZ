81e50f87acc826fa96513cfb32b32f55d610c862
TosoxDev <benjamingriller@gmail.com>
2021-03-13 00:06:38 +0100

    sa

--- Header/Offsets.h
+++ Header/Offsets.h
@@ -10,6 +10,6 @@ void GetOffsets(uintptr_t gameModule) {
 	std::cout << "getting lp" << std::endl;
 	dwLocalPlayer = (FindPattern("client.dll", "8D 34 85 ? ? ? ? 89 15 ? ? ? ? 8B 41 08 8B 48 04 83 F9 FF") + 0x7);
 	std::cout << std::to_string(dwLocalPlayer) << std::endl;
-	std::cout << std::to_string(lPlayer) << std::endl;
+	std::cout << std::to_string(lPlayer) << std::endl; 
 	std::cout << std::to_string(gameModule + lPlayer) << std::endl;
 }
